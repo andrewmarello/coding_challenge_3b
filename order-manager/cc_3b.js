77038be8d92100ba004faa2c5b920f8c5bc9c1c1
Andrew <andrewmarello@usf.edu>
2026-02-08 17:41:52 -0500

    Inventory Inputs

--- order-manager/cc_3b.js
+++ order-manager/cc_3b.js
@@ -1,1 +1,31 @@
 console.log(`Hello! This is Coding Challenge 3b - Order Manager - Andrew Marello`);
+
+// Step 2: Model the Inventory
+let inventory = [
+    {
+        sku: "SKU-001",
+        name: "Blanket",
+        price: 34.99,
+        stock: 50
+    },
+    {
+        sku: "SKU-002",
+        name: "Pillow",
+        price: 19.99,
+        stock: 100
+    },
+    {
+        sku: "SKU-003",
+        name: "Bed Sheet",
+        price: 29.99,
+        stock: 75
+    },
+    {
+        sku: "SKU-004",
+        name: "Comforter",
+        price: 49.99,
+        stock: 40
+    }
+];
+
+//Step 3: Manage Inventory Changes
